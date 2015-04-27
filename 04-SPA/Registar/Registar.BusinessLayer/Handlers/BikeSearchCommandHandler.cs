6bbc09ddc02ef436cf6284e70ed4e257b0d68f5a
Dragica <dragican@live.com>
2015-04-27 11:49:16 +0200

    reg app

---
+++ 04-SPA/Registar/Registar.BusinessLayer/Handlers/BikeSearchCommandHandler.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Registar.BusinessLayer.Contracts;
+using Registar.DataLayer;
+using Registar.DomainModel;
+
+namespace Registar.BusinessLayer.Handlers
+{
+    internal class BikeSearchCommandHandler:CommandHandlerBase<BikeSearchCommand,BikeSearchResult>
+    {
+        protected override BikeSearchResult ExecuteCommand(BikeSearchCommand command)
+        {
+            using (RegistarDbContext context = new RegistarDbContext())
+            {
+                //IEnumerable<Bike> bikes = new List<Bike>();
+                //bikes = context.Bikes
+                //        .OrderBy(p => p.BikeId)
+                //        .Take(10);
+                        //.ToList();
+                
+                var query = from b in context.Bikes
+                           select b;
+                
+                if (!string.IsNullOrEmpty(command.Colour))
+                {
+                    query = query.Where(x => x.Colour == command.Colour);
+                }
+                if (!string.IsNullOrEmpty(command.Producer))
+                {
+                    query = query.Where(x => x.Producer == command.Producer);
+                }
+
+                query = query
+                        .OrderBy(x => x.BikeId);
+                //
+                
+                BikeSearchResult result = new BikeSearchResult();
+                result.Result = query.ToList();
+                return result;
+            }
+            
+        }
+    }
+}
---
+++ 04-SPA/Registar/Registar/Controllers/HomeController.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Registar.BusinessLayer;
+using Registar.BusinessLayer.Contracts;
+using Registar.Models;
+using Registar.DataLayer;
+
+namespace Registar.Controllers
+{
+    public class HomeController : Controller
+    {
+        //
+        // GET: /Home/
+
+        public ActionResult Index()
+        {
+            //call BL
+            BikeSearchCommand _command = new BikeSearchCommand();
+            BikeSearchResult _result = CommandInvoker.InvokeCommand<BikeSearchCommand, BikeSearchResult>(_command);
+            //
+            return View(_result.Result);
+        }
+
+        public ActionResult Index2()
+        {
+            List<BikeModel> _result = new List<BikeModel>();
+            _result.Add(new BikeModel() { Colour = "red", Model = "R1", Producer = "Specialized", RegNumber = "007" });
+            _result.Add(new BikeModel() { Colour = "red", Model = "R1", Producer = "Specialized", RegNumber = "007" });
+            _result.Add(new BikeModel() { Colour = "red", Model = "R1", Producer = "Specialized", RegNumber = "007" });
+            _result.Add(new BikeModel() { Colour = "red", Model = "R1", Producer = "Specialized", RegNumber = "007" });
+            //
+            this.ViewBag.SomeNewProperty = "theValue";
+            this.ViewData["SomeNewProperty"] = "theValue";
+            //
+            return View("Index",_result);
+        }
+
+        public ActionResult View1()
+        {
+            RegistarDbContext context = new RegistarDbContext();
+            var model = context.Bikes.ToList();
+
+            return View(model);
+        }
+
+    }
+}
